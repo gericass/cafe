fc6633a675b60cd9753764b428dec81ee963992c
gericass <bonjovi1926@yahoo.co.jp>
2018-04-26 09:36:51 +0900

    apply library

---
+++ view/src/Common/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Header extends React.Component {
+  public render() {
+    return (
+      <div className="header">
+      </div>
+    );
+  }
+}
+
+export default Header;
